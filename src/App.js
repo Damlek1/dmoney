ae0e95afbf138e8587f1d3eb0aa07bd00bd1e4b1
Damlek1 <100087318+Damlek1@users.noreply.github.com>
2022-08-18 18:30:00 +0100

    Hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import {BrowserRouter} from 'react-router-dom'
 import Home from './pages/Home'
+import Hero from './components/HeroSection/Hero'
 
 function App() {
   return (
     <BrowserRouter>
       <Home />
+      <Hero />
     </BrowserRouter>
   );
 }
