7d62168317faa0d9058898e3b5e8550154d287d2
Damlek1 <100087318+Damlek1@users.noreply.github.com>
2022-08-24 19:47:40 +0100

    Footer commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from 'react-icons/fa'
+import {FooterContainer, FooterWrap,FooterLinksContainer,FooterLinksWrapper,FooterLinkItems, FooterLinkTitle,FooterLink,SocialMedia,SocialMediaWrap,SocialLogo,WebsiteRights,SocialIconLink, SocialIcons} from './index'
+
 
 const Footer = () => {
   return (
@@ -7,40 +10,61 @@ const Footer = () => {
         <FooterWrap>
             <FooterLinksContainer>
                 <FooterLinksWrapper>
-                    <FooterLinksItems>
+                    <FooterLinkItems>
                         <FooterLinkTitle>About us</FooterLinkTitle>
-                            <FooterLink to='/signin'>How it works </FooterLink>
-                            <FooterLink to='/signin'>Testimonials</FooterLink>
-                            <FooterLink to='/signin'>Careers</FooterLink>
-                            <FooterLink to='/signin'>Investors</FooterLink>
-                            <FooterLink to='/signin'>Terms of Service</FooterLink>
-                    </FooterLinksItems>
-                    <FooterLinksItems>
-                        <FooterLinkTitle>About us</FooterLinkTitle>
-                            <FooterLink to='/signin'>How it works </FooterLink>
-                            <FooterLink to='/signin'>Testimonials</FooterLink>
-                            <FooterLink to='/signin'>Careers</FooterLink>
-                            <FooterLink to='/signin'>Investors</FooterLink>
-                            <FooterLink to='/signin'>Terms of Service</FooterLink>
-                    </FooterLinksItems>
-                    <FooterLinksItems>
-                        <FooterLinkTitle>About us</FooterLinkTitle>
-                            <FooterLink to='/signin'>How it works </FooterLink>
-                            <FooterLink to='/signin'>Testimonials</FooterLink>
-                            <FooterLink to='/signin'>Careers</FooterLink>
-                            <FooterLink to='/signin'>Investors</FooterLink>
-                            <FooterLink to='/signin'>Terms of Service</FooterLink>
-                    </FooterLinksItems>
-                    <FooterLinksItems>
-                        <FooterLinkTitle>About us</FooterLinkTitle>
-                            <FooterLink to='/signin'>How it works </FooterLink>
-                            <FooterLink to='/signin'>Testimonials</FooterLink>
-                            <FooterLink to='/signin'>Careers</FooterLink>
-                            <FooterLink to='/signin'>Investors</FooterLink>
-                            <FooterLink to='/signin'>Terms of Service</FooterLink>
-                    </FooterLinksItems>
+                            <FooterLink to='/'>How it works </FooterLink>
+                            <FooterLink to='/'>Testimonials</FooterLink>
+                            <FooterLink to='/'>Careers</FooterLink>
+                            <FooterLink to='/'>Investors</FooterLink>
+                            <FooterLink to='/'>Terms of Service</FooterLink>
+                    </FooterLinkItems>
+                    <FooterLinkItems>
+                        <FooterLinkTitle>Contact us</FooterLinkTitle>
+                            <FooterLink to='/'>Contact </FooterLink>
+                            <FooterLink to='/'>Support</FooterLink>
+                            <FooterLink to='/'>Destinations</FooterLink>
+                            <FooterLink to='/'>Sponsorships</FooterLink>
+                    </FooterLinkItems>
+                    <FooterLinkItems>
+                        <FooterLinkTitle>Videos</FooterLinkTitle>
+                            <FooterLink to='/'>Submit Videos</FooterLink>
+                            <FooterLink to='/'>Ambassadors</FooterLink>
+                            <FooterLink to='/'>Agency</FooterLink>
+                            <FooterLink to='/'>Influencer</FooterLink>
+                    </FooterLinkItems>
+                    <FooterLinkItems>
+                        <FooterLinkTitle>Social Media</FooterLinkTitle>
+                            <FooterLink to='/'>Instagram</FooterLink>
+                            <FooterLink to='/'>Facebook</FooterLink>
+                            <FooterLink to='/'>Youtube</FooterLink>
+                            <FooterLink to='/'>Twitter</FooterLink>
+                    </FooterLinkItems>
                 </FooterLinksWrapper>
             </FooterLinksContainer>
+            <SocialMedia>
+                <SocialMediaWrap>
+                    <SocialLogo to='/'>dolla</SocialLogo>
+                    <WebsiteRights>dolla &copy; {new Date().getFullYear()} All rights reserved.</WebsiteRights>
+                    <SocialIcons>
+
+                    <SocialIconLink href='//www.facebook.com' target='_blank' aria-label='Facebook'>
+                        <FaFacebook />
+                    </SocialIconLink>
+                    <SocialIconLink href='//www.instagram.com' target='_blank' aria-label='Instagram'>
+                        <FaInstagram />
+                    </SocialIconLink>
+                    <SocialIconLink href='//www.youtube.com' target='_blank' aria-label='Youtube'>
+                        <FaYoutube />
+                    </SocialIconLink>
+                    <SocialIconLink href='//www.twitter.com' target='_blank' aria-label='Twitter'>
+                        <FaTwitter />
+                    </SocialIconLink>
+                    <SocialIconLink href='//www.linkedin.com/in/damlek1' target='_blank' aria-label='Linkedin'>
+                        <FaLinkedin />
+                    </SocialIconLink>
+                    </SocialIcons>
+                </SocialMediaWrap>
+            </SocialMedia>
         </FooterWrap>
     </FooterContainer>
     </>
